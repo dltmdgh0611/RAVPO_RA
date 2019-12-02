2cbdf81b4b928abb69c95f78d656ad83727c770b
Lee_seung_ho <38284097+dltmdgh0611@users.noreply.github.com>
2019-12-02 15:44:28 +0900

    v0.0.2
    already finish??

--- RAVPO_RA/RAVPO_RA/RAVPO_RA.ino
+++ RAVPO_RA/RAVPO_RA/RAVPO_RA.ino
@@ -3,13 +3,33 @@
  Created:	2019-12-02 오후 12:26:29
  Author:	user
 */
-
 // the setup function runs once when you press reset or power the board
-void setup() {
 
+#include "RAVPO.h"
+INITS initmode;
+MOVING moving;
+void setup() {
+	initmode.j1E = true;
+	initmode.j2E = true;
+	initmode.roE = true;
+
+	initmode.RAVPO_init(initmode, 1000);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	moving.MOVING_XY(500, 500);
+	delay(1000);
+	moving.MOVING_XY(0, 0);
+	delay(1000);
+}
+
+
+SIGNAL(TIMER1_COMPA_vect) {
+	moving.RUNNING_STEPMOTOR(MOVING_SELECT_X);
+}
+
+
+SIGNAL(TIMER3_COMPA_vect) {
+	moving.RUNNING_STEPMOTOR(MOVING_SELECT_Y);
 }
